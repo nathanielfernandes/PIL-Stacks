ef1a1d9db5c63fa6819de5b02be558b88f351101
fabyanMikhael <fabyanmikhael@yahoo.ca>
2021-05-23 00:06:16 -0400

    fixed example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 from src import *
 import os
 
-a = Editor(name="example", Background="src/pil_stacks/Assets/example_background.png")
+a = Editor(name="example", Background="src/pil_stacks/Assets/example_background.png", Background_as_constant=True)
 # a.show_fps = True
 # a.__debug_info__ = True
 
